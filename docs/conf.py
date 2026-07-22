307506f02d97d00fd0d07287798285f32b34b6e3
yjkhfuh <yjkhfuh89@outlook.com>
2026-07-22 11:06:13 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'bitdefender-password-manager-login'
+author = 'bitdefender-password-manager-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
